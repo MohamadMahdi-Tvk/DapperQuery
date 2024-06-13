ba1a1c138766132d2ec088ee8c9abbf0bbf52bfa
mohamadmahditavakoli <mohamadmahditavakoli8@gmail.com>
2024-06-13 15:42:36 +0330

    Feat: 1- One To One Join 2- Multiple Queries 3- Other Queries In Dapper 4- Use StoredProcedure In Dapper

--- DapperQuery/Program.cs
+++ DapperQuery/Program.cs
@@ -4,16 +4,50 @@ Console.WriteLine("Hello, World!");
 
 Repository repository = new Repository();
 
+#region Add
+
 //Console.WriteLine(repository.Add());
 
-var orders = repository.GetOrders();
+#endregion
 
-foreach (var item in orders)
-{
-    Console.WriteLine($"Order: {item.Id} On Date: {item.Date}");
+#region GetOrders
 
-    foreach (var detail in item.OrderDetails)
-    {
-        Console.WriteLine($"Id: {detail.Id} {detail.ProductName}");
-    }
-}
+//var orders = repository.GetOrders();
+
+//foreach (var item in orders)
+//{
+//    Console.WriteLine($"Order: {item.Id} On Date: {item.Date}");
+
+//    foreach (var detail in item.OrderDetails)
+//    {
+//        Console.WriteLine($"Id: {detail.Id} {detail.ProductName}");
+//    }
+//}
+
+#endregion
+
+#region GetInvoice
+
+var orders = repository.GetInvoice();
+
+#endregion
+
+#region MultipleQueries
+
+repository.MultipleQueries();
+
+#endregion
+
+#region Queries
+
+//repository.Queries();
+
+#endregion
+
+#region RunStoredProcedure
+
+//var ordersFromProc = repository.RunSp();
+
+#endregion
+
+Console.ReadKey();
--- DapperQuery/Repository.cs
+++ DapperQuery/Repository.cs
@@ -1,4 +1,5 @@
 ﻿using Dapper;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace DapperQuery;
@@ -7,6 +8,7 @@ public class Repository
 {
     private readonly string connectionString = "Server=.;Initial Catalog=Dapper_Db; Integrated Security=true; TrustServerCertificate=true";
 
+    //Add
     public int Add()
     {
         string sql = "Insert Into Customers (FirstName, LastName) Values(@FirstName, @LastName); Select SCOPE_IDENTITY()";
@@ -19,22 +21,22 @@ public class Repository
     }
 
     //Join One To Many
-    public List<Order> GetOrders()
+    public List<Orders> GetOrders()
     {
         string sql = "Select Top 10 * From Orders As O Inner Join OrderDetails As OD On O.Id = OD.OrderId";
 
         var connection = new SqlConnection(connectionString);
 
-        var orderDic = new Dictionary<long, Order>();
+        var orderDic = new Dictionary<long, Orders>();
 
-        var orderList = connection.Query<Order, OrderDetail, Order>(sql,
+        var orderList = connection.Query<Orders, OrderDetails, Orders>(sql,
             (order, orderDetail) =>
             {
-                Order entity;
+                Orders entity;
                 if (!orderDic.TryGetValue(order.Id, out entity))
                 {
                     entity = order;
-                    entity.OrderDetails = new List<OrderDetail>();
+                    entity.OrderDetails = new List<OrderDetails>();
                     orderDic.Add(entity.Id, entity);
                 }
                 entity.OrderDetails.Add(orderDetail);
@@ -47,19 +49,89 @@ public class Repository
 
         return orderList;
     }
+
+    //Join One To One
+    public List<Orders> GetInvoice()
+    {
+        string sql = "SELECT * FROM Orders As O INNER JOIN Invoice As I ON O.Id = I.Id";
+
+        var connection = new SqlConnection(connectionString);
+
+        var invoice = connection.Query<Orders, Invoice, Orders>(sql,
+            (order, invoice) =>
+            {
+                order.Invoice = invoice;
+                return order;
+
+            }, splitOn: "Id").Distinct().ToList();
+
+        return invoice;
+
+    }
+
+    //Multiple Queries
+    public void MultipleQueries()
+    {
+        string sql = "Select * From Orders; Select * From Invoice";
+
+        var connection = new SqlConnection(connectionString);
+
+        var result = connection.QueryMultiple(sql);
+
+        var orders = result.Read<Orders>().ToList();
+
+        var invoice = result.Read<Invoice>().ToList();
+    }
+
+    //QueryFirst, QyeryFirstOrDefault, QuerySingle, QuerySingleOrDefault
+    public void Queries()
+    {
+        string sql = "Select * From Orders";
+
+        var connection = new SqlConnection(connectionString);
+
+        var resultQueryFirst = connection.QueryFirst<Orders>(sql);
+
+        var resultQueryFirstOrDefault = connection.QueryFirstOrDefault<Orders>(sql);
+
+        var resultQueySingle = connection.QuerySingle<Orders>(sql);
+
+        var resultQuerySingleOrDefault = connection.QuerySingleOrDefault<Orders>(sql);
+    }
+
+    //Use Stored Procedures
+    public List<Orders> RunSp()
+    {
+        string procedureName = "SelectOrders";
+
+        var connection = new SqlConnection(connectionString);
+
+        var result = connection.Query<Orders>(procedureName, CommandType.StoredProcedure);
+
+        return result.ToList();
+    }
+
 }
 
-public class Order
+public class Orders
 {
     public int Id { get; set; }
     public DateTime Date { get; set; }
-    public List<OrderDetail> OrderDetails { get; set; }
+    public List<OrderDetails> OrderDetails { get; set; }
+
+    public Invoice Invoice { get; set; }
 }
 
-public class OrderDetail
+public class OrderDetails
 {
     public int Id { get; set; }
     public int OrderId { get; set; }
     public string ProductName { get; set; }
+}
+
+public class Invoice
+{
+    public int Id { get; set; }
+    public long Price { get; set; }
 }
 
